15d32797dc5278343e001be9a812ca5b071466f5
asanson <alexandresanson.pro@gmail.com>
2023-12-07 23:36:28 +0100

    feat: add JWT Token in a Secret file and access it from an handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-use axum::{routing::get, Router};
+use axum::{extract::State, routing::get, Router};
+use shuttle_secrets::SecretStore;
+use std::sync::Arc;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn print_jwt_token(State(state): State<Arc<AppState>>) -> String {
+    state.jwt.clone()
+}
+
+#[derive(Clone)]
+struct AppState {
+    jwt: String,
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+async fn main(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+
+    let Some(secret) = secret_store.get("MY_JWT_TOKEN") else {
+        panic!("API KEY NOT FOUND")
+    };
+
+    let state = Arc::new(AppState {
+        jwt: secret,
+    });
+
+    
+    let router = Router::new().route("/", get(print_jwt_token)).with_state(state);
 
     Ok(router.into())
 }
